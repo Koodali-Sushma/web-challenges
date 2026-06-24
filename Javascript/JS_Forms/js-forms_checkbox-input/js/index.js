ecabf31af2591a27f64ec8d37c60f8f4071aa59d
Sushma Koodali <sushma.gargya@outlook.com>
2026-06-24 16:52:38 +0200

    JS_Form Checkbox challenge completed

--- Javascript/JS_Forms/js-forms_checkbox-input/js/index.js
+++ Javascript/JS_Forms/js-forms_checkbox-input/js/index.js
@@ -4,32 +4,49 @@ const form = document.querySelector('[data-js="form"]');
 const successMessage = document.querySelector('[data-js="success"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const firstName = document.querySelector('[data-js="firstName"]');
+
+//.hide checkbox error message and success message
+hideTosError();
+hideSuccess();
+
+function showSuccess() {
+  successMessage.removeAttribute("hidden");
+}
+function hideSuccess() {
+  successMessage.setAttribute("hidden", "");
+}
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
-  successMessage.setAttribute("hidden", "");
 }
 
 function showTosError() {
   tosError.removeAttribute("hidden");
-  successMessage.removeAttribute("hidden");
 }
-hideTosError(); //By default hiding the error message
-
-////  hide error message once checkbox selected
-tosCheckbox.addEventListener("change", () => {
-  if (!tosCheckbox.checked) showTosError();
-  else hideTosError();
-});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
-  if (!tosCheckbox.checked) showTosError();
-  else hideTosError();
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
+  if (!tosCheckbox.checked) {
+    hideSuccess();
+    showTosError();
+  } else {
+    hideTosError();
+    showSuccess();
+  }
   alert("Form submitted");
+  event.target.reset();
+  firstName.focus();
+});
+
+////  hide error message once checkbox selected
+tosCheckbox.addEventListener("change", () => {
+  if (!tosCheckbox.checked) {
+    hideSuccess();
+    showTosError();
+  } else {
+    showSuccess();
+    hideTosError();
+  }
 });
