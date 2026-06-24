9ff1b4e1f0673d812c581abfaa80d810c6a30d75
Sushma Koodali <sushma.gargya@outlook.com>
2026-06-24 22:31:16 +0200

    JS_Forms calculator challenge completed

--- Javascript/JS_Forms/js-forms_calculator/js/index.js
+++ Javascript/JS_Forms/js-forms_calculator/js/index.js
@@ -25,6 +25,29 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const numberA = Number(data.numberA);
+  const numberB = Number(data.numberB);
+  const operator = data.operator;
+
+  switch (operator) {
+    case "addition":
+      result = add(numberA, numberB);
+      break;
+    case "subtraction":
+      result = subtract(numberA, numberB);
+      break;
+    case "multiplication":
+      result = multiply(numberA, numberB);
+      break;
+    case "division":
+      result = divide(numberA, numberB);
+      break;
+  }
 
   // --^-- write your code here --^--
 
