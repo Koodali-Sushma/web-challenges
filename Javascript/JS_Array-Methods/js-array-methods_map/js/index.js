2f0288d0253eb06259cdd09aad5c2f54b80ab0de
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-02 22:48:44 +0200

    JS_ArrayMethods challenges 1,2,3 completed

---
+++ Javascript/JS_Array-Methods/js-array-methods_map/js/index.js
@@ -0,0 +1,43 @@
+console.clear();
+
+const cards = [
+  {
+    id: "1",
+    isBookmarked: false,
+    question: "How often can I use <header>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "2",
+    isBookmarked: false,
+    question: "How often can I use <aside>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "3",
+    isBookmarked: true,
+    question: "On which types can I use destructuring assignment?",
+    answer: "On Objects and Arrays",
+    tags: ["js", "next", "advanced"],
+  },
+];
+// ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => card.answer.toLowerCase());
+console.log("lowerCaseAnswers =", lowerCaseAnswers);
+
+// ["How often can I use <header>? - As often as you like.", ...]
+//prettier-ignore
+const questionsAndAnswersTogether = cards.map(
+  (card) => `${card.question} - ${card.answer}`
+);
+console.log("questionsAndAnswersTogether =", questionsAndAnswersTogether);
+
+// [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  return { question: card.question, answer: card.answer };
+});
+console.log("questionAndAnswer =", questionAndAnswer);
+
+//export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
