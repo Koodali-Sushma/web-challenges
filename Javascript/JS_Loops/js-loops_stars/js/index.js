59429f2556d1d79111352051bbf486754bb0e798
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-02 23:05:35 +0200

    Stars challenge only stars displayed, filling starts pending and pixel challenge pending

--- Javascript/JS_Loops/js-loops_stars/js/index.js
+++ Javascript/JS_Loops/js-loops_stars/js/index.js
@@ -7,7 +7,11 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const imgElement = document.createElement("img");
+    imgElement.src = "./assets/star-empty.svg";
+    starContainer.append(imgElement);
+  }
   // --^-- write or modify code above this line --^--
 }
 
