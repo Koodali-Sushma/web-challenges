0f0e7c8ba137ec4a0eb4b957967de4e39414cc90
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-06 16:31:29 +0200

    Challenge Star-wars-API-cards

--- Javascript/JS_Fetch/js-fetch_star-wars/js/index.js
+++ Javascript/JS_Fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,15 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  const data = await response.json();
+  console.log("data: ", data);
+  const personList = data.results;
+
+  personList.forEach((person) => {
+    if (person.name === "Luke Skywalker") return;
+    const card = Card(person);
+    renderElement(card);
+  });
 }
