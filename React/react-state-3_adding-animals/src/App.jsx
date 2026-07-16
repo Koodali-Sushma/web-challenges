c9ee3517d034274f5ca23c99135c3b23f59a8709
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-16 16:22:23 +0200

    adding animals challenge React State -3 completed

---
+++ React/react-state-3_adding-animals/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form";
+import List from "./components/List";
+import { uid } from "uid";
+
+const initialAnimals = [
+  {
+    id: "mTBErig",
+    name: "Octopus",
+    emoji: "🐙",
+  },
+  {
+    id: "iMsbXQ1",
+    name: "Crocodile",
+    emoji: "🐊",
+  },
+  {
+    id: "Qjf_K7V",
+    name: "Beaver",
+    emoji: "🦫",
+  },
+];
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  function handleAddAnimal(newAnimal) {
+    //  Set the state with a fresh array
+    setAnimals([
+      ...animals, // Copy all existing animals from state
+      {
+        id: uid(), // Generate a fresh unique ID
+        ...newAnimal, // Spread the rest of the submitted animal fields (name, emoji, etc.)
+      },
+    ]);
+  }
+
+  return (
+    <main className="app">
+      <Form onAddAnimal={handleAddAnimal} />
+      <List animals={animals} />
+    </main>
+  );
+}
